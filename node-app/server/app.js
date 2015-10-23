566e5e335008470488f0b4129c214b385cb5c282
Brian Thompson <brian@blendlabs.com>
2015-10-23 12:19:44 -0700

    Setup root.

--- node-app/server/app.js
+++ node-app/server/app.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
 
+app.use('/', express.static(__dirname + '/../app'));
 app.use('/app', express.static(__dirname + '/../app'));
 
 var server = app.listen(8000, function () {
